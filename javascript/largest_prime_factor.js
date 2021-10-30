ad70490f9176e67c75f38b96287fb85ef92f6d3e
Berqia Mouad <83679093+BerqiaMouad@users.noreply.github.com>
2021-10-30 14:32:53 +0100

    Add files via upload

---
+++ javascript/largest_prime_factor.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+});
+
+function fct() {
+    var output;
+    var inpt = answer.value;
+    if(inpt == "6857"){
+        output = "Correct!";
+        document.getElementById("message").innerHTML = output;
+        document.getElementById("incorrectBtn").style.display = 'none';
+        document.getElementById("correctBtn").style.display = 'inline';
+    }
+    else{
+        output = "Incorrect!";
+        document.getElementById("message").innerHTML = output;
+        document.getElementById("incorrectBtn").style.display = 'inline';
+        document.getElementById("correctBtn").style.display = 'none';
+    }
+}
